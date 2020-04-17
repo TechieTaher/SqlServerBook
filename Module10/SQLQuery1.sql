7353e12b4967a062eeab54eb3ef35897fdc6189f
Techie Taher <dahodwalataher458@gmail.com>
2020-04-17 06:02:56 +0530

    Module 10,11

---
+++ Module10/SQLQuery1.sql
@@ -0,0 +1,44 @@
+CREATE FUNCTION dbo.FormatPhoneNumber
+( @PhoneNumberToFormat nvarchar(16)
+)
+RETURNS nvarchar(16)
+AS
+BEGIN
+	DECLARE @Digits nvarchar(16) = '';
+	DECLARE @Remaining nvarchar(16) = @PhoneNumberToFormat;
+	DECLARE @Character nchar(1);
+	
+	IF LEFT(@Remaining,1) = N'+' RETURN @Remaining;
+	
+	WHILE (LEN(@Remaining) > 0) BEGIN
+		SET @Character = LEFT(@Remaining,1);
+		SET @Remaining = SUBSTRING(@Remaining,2,LEN(@Remaining));
+		IF (@Character >= N'0') AND (@Character <= N'9')
+			SET @Digits += @Character;
+	END;
+	RETURN CASE LEN(@Digits)
+		WHEN 10 THEN N'(' + SUBSTRING(@Digits,1,3) + N') '
+			+ SUBSTRING(@Digits,4,3) + N'-'
+			+ SUBSTRING(@Digits,7,4)
+		WHEN 8 THEN SUBSTRING(@Digits,1,4) + N'-'
+			+ SUBSTRING(@Digits,5,4)
+		WHEN 6 THEN SUBSTRING(@Digits,1,3) + N'-'
+		+ SUBSTRING(@Digits,4,3)ELSE @Digits
+	END;
+END;
+
+SELECT dbo.FormatPhoneNumber('123456789');
+
+
+Create FUNCTION dbo.IntegerListToTable 
+( @fullName nvarchar(30), @delimiter nvarchar(1))
+RETURNS @result TABLE (firstName nvarchar(100))
+AS BEGIN
+	INSERT INTO @result (firstName)
+	SELECT value from STRING_SPLIT ( @fullName , @delimiter );
+	RETURN;
+END;
+
+select * from dbo.IntegerListToTable ('John Doe',' ')
+
+
